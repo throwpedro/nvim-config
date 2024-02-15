c6ed67db3b1d46b20be4ba03d20d770c778b925c
thor <thor@colourbox.com>
2024-02-15 22:34:06 +0100

    Default to open with neotree opened. Add rubocop to none-ls highliting. Auto detext syntax in treesitter

--- init.lua
+++ init.lua
@@ -1,14 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.api.nvim_create_autocmd("VimEnter", {
+  command = "set nornu nonu | Neotree toggle",
+})
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
