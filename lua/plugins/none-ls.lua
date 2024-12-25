c84cc596aa213a0e3bb05a08aa2613421a3aeca1
Thor <thordoor89@hotmail.com>
2024-12-25 19:30:13 +0100

    feat: remove ruby

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,8 +6,6 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.rubocop,
       },
     })
 
