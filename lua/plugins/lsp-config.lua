7b590cc4a398307dd3108d29f441708273f00c20
Thor <thordoor89@hotmail.com>
2024-12-25 19:22:00 +0100

    feat: added gopls lsp for golang

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" }
+        ensure_installed = { "lua_ls", "ts_ls", "gopls" }
       })
     end
   },
@@ -18,6 +18,7 @@ return {
     config = function()
       local lspconfig = require('lspconfig')
       lspconfig.ts_ls.setup({})
+      lspconfig.gopls.setup({})
       lspconfig.lua_ls.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
