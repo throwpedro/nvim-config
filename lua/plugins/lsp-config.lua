966886e67ed2918bc157ea919d20d80e8be345a8
thor <thordoor89@hotmail.com>
2025-02-11 22:05:53 +0100

    feat: added code completion with nvim-cmp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,27 +3,31 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "gopls" }
+        ensure_installed = { "lua_ls", "ts_ls", "gopls" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.ts_ls.setup({ filetypes = { "javascript", "typescript", "typescriptreact", "typescript.tsx" } })
-      lspconfig.gopls.setup({})
-      lspconfig.lua_ls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-    end
-  }
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.ts_ls.setup({
+        filetypes = { "javascript", "typescript", "typescriptreact", "typescript.tsx" },
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({ capabilities = capabilities})
+      lspconfig.lua_ls.setup({ capabilities = capabilities})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+    end,
+  },
 }
