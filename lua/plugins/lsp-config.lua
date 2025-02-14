cfa7b8dc15abb282a743f2b737bc040425282b78
thor <thordoor89@hotmail.com>
2025-02-14 19:21:45 +0100

    feat: added luasnip snippet engine so snippets work in golang

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,8 +22,8 @@ return {
         filetypes = { "javascript", "typescript", "typescriptreact", "typescript.tsx" },
         capabilities = capabilities,
       })
-      lspconfig.gopls.setup({ capabilities = capabilities})
-      lspconfig.lua_ls.setup({ capabilities = capabilities})
+      lspconfig.gopls.setup({ capabilities = capabilities,})
+      lspconfig.lua_ls.setup({ capabilities = capabilities,})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
